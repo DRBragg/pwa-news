162a3c9a0014ff9010d3ccefc2cbfbda0e9eec0c
DRBragg <DRBragg@gmail.com>
2018-02-08 11:21:15 -0500

    updated service worker to handle news stories

--- sw.js
+++ sw.js
@@ -12,11 +12,32 @@ self.addEventListener('install', async (e) => {
 // Intercept HTTP requests
 self.addEventListener('fetch', (e) => {
   const req = e.request;
-  e.respondWith(cacheFirst(req));
+  const url = new URL(req.url);
+
+  // If request is requesting static assets serve cached assets
+  // If request is requesting news stories check cache
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkFirst(req));
+  }
 });
 
 // Serve cached assets if they are saved otherwise fetch assets
 async function cacheFirst(req) {
   const cachedResponse = await caches.match(req);
   return cachedResponse || fetch(req);
+}
+
+// Try to fetch news stories and cache them otherwise serve cached stories
+async function networkFirst(req) {
+  const cache = await caches.open('news-dynamic');
+
+  try {
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch (error) {
+    return await cache.match(req);
+  }
 }
